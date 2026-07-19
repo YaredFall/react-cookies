89f8aaec1fec4acf161f87c38ff21ddb252c0992
YaredFall <yaredfall@gmail.com>
2026-07-19 19:31:31 +0700

    refactor: rename useCookie option from `default` to `defaultValue`

--- src/use-cookie.ts
+++ src/use-cookie.ts
@@ -7,7 +7,7 @@ import { useCookieStore } from "./use-context";
 
 export type UseCookieOptions<T = unknown> = SetCookieOptions & {
     /** The default value to return if the cookie is not found */
-    default?: T;
+    defaultValue?: T;
     /**  The function to parse the cookie value */
     parse?: ParseCookieValue<T>;
     /**  The function to stringify the cookie value */
@@ -17,7 +17,7 @@ export type UseCookieOptions<T = unknown> = SetCookieOptions & {
 export function useCookie<T = unknown>(name: string, options: UseCookieOptions<T> = {}) {
     const store = useCookieStore();
 
-    const getSnapshot = () => store.getCookie(name, options.parse) ?? options.default;
+    const getSnapshot = () => store.getCookie(name, options.parse) ?? options.defaultValue;
 
     const value = useSyncExternalStore(store.subscribe, getSnapshot, getSnapshot);
 
