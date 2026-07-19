cd2b046eb40ca5da80d3801d889d1fe2c7c678c6
YaredFall <yaredfall@gmail.com>
2026-07-19 20:41:13 +0700

    refactor: extract defaultValue from options to the second argument

--- src/use-cookie.ts
+++ src/use-cookie.ts
@@ -6,23 +6,21 @@ import type { SetCookieOptions } from "./store";
 import { useCookieStore } from "./use-context";
 
 export type UseCookieOptions<T = unknown> = SetCookieOptions & {
-    /** The default value to return if the cookie is not found */
-    defaultValue?: T;
     /**  The function to parse the cookie value */
     parse?: ParseCookieValue<T>;
     /**  The function to stringify the cookie value */
     stringify?: StringifyCookieValue<T>;
 };
 
-export function useCookie<T = unknown>(name: string, options: UseCookieOptions<T> = {}) {
+export function useCookie<T>(name: string, defaultValue?: T, options?: UseCookieOptions<T>) {
     const store = useCookieStore();
 
-    const getSnapshot = () => store.getCookie(name, options.parse) ?? options.defaultValue;
+    const getSnapshot = () => store.getCookie(name, options?.parse) ?? defaultValue;
 
     const value = useSyncExternalStore(store.subscribe, getSnapshot, getSnapshot);
 
     const set = useCallback(
-        (value: T) => store.setCookie(name, value, options, options.stringify),
+        (value: T) => store.setCookie(name, value, options, options?.stringify),
         [store, name, options],
     );
 
